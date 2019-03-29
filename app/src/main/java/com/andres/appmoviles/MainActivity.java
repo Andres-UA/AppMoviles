81af48c6fe1f0ddde882a20352b86641d5caf536
Prestamo 0820 <scm08e20@icesi.edu.co>
2019-03-28 21:00:47 -0500

    Camera

--- app/src/main/java/com/andres/appmoviles/MainActivity.java
+++ app/src/main/java/com/andres/appmoviles/MainActivity.java
@@ -1,6 +1,8 @@
 package com.andres.appmoviles;
 
+import android.Manifest;
 import android.content.Intent;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -25,6 +27,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActivityCompat.requestPermissions(this, new String[]{
+                Manifest.permission.CALL_PHONE,
+                Manifest.permission.CAMERA,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+        }, 0);
 
         localdb = DBHandler.getInstance(this);
         auth = FirebaseAuth.getInstance();
--- app/src/main/java/com/andres/appmoviles/NewFriendActivity.java
+++ app/src/main/java/com/andres/appmoviles/NewFriendActivity.java
@@ -1,28 +1,44 @@
 package com.andres.appmoviles;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.os.StrictMode;
 import android.preference.PreferenceManager;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.andres.appmoviles.db.DBHandler;
 import com.andres.appmoviles.model.Friend;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.UUID;
 
 public class NewFriendActivity extends AppCompatActivity {
 
+    private static final int CAMERA_CALLBACK_ID = 100;
     private EditText etName;
     private EditText etAge;
     private EditText etEmail;
     private EditText etPhone;
+    private ImageView ivPicture;
 
     private Button btnSaveFriend;
+    private Button btnTakePic;
+
+    private File photoFile;
+
 
     DBHandler db;
 
@@ -31,12 +47,17 @@ public class NewFriendActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_friend);
 
+        // Si no pongo esto entonces va a salir el URIFileExposedException
+        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+        StrictMode.setVmPolicy(builder.build());
+
         db = DBHandler.getInstance(this);
 
         etName = findViewById(R.id.et_name);
         etAge = findViewById(R.id.et_age);
         etEmail = findViewById(R.id.et_email);
         etPhone = findViewById(R.id.et_phone);
+        ivPicture = findViewById(R.id.iv_image_friend);
 
         btnSaveFriend = findViewById(R.id.btn_save_friend);
         btnSaveFriend.setOnClickListener(new View.OnClickListener() {
@@ -54,6 +75,28 @@ public class NewFriendActivity extends AppCompatActivity {
 
             }
         });
+
+        btnTakePic = findViewById(R.id.btn_take_pic);
+        btnTakePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                photoFile = new File(Environment.getExternalStorageDirectory() + "/" + UUID.randomUUID().toString() + ".png");
+                Uri uri = Uri.fromFile(photoFile);
+                i.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+                startActivityForResult(i, CAMERA_CALLBACK_ID);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        // Luego de tomar la foto y guardarla
+        if (requestCode == CAMERA_CALLBACK_ID && resultCode == RESULT_OK) {
+            Bitmap imagen = BitmapFactory.decodeFile(photoFile.toString());
+            ivPicture.setImageBitmap(imagen);
+        }
     }
 
     // Cargar información
